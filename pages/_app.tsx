ffd2876e7c2f6789bd985be18a273263d2f1a4c1
Nicholas Kostelnik <nitaigao@users.noreply.github.com>
2020-12-19 14:33:17 +0400

    fixup function

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,8 +4,6 @@ import '../styles/globals.css'
 
 import type { AppProps } from 'next/app'
 
-const App = ({ Component, pageProps }: AppProps): ReactElement => {
-  return <Component {...pageProps} />
-}
+const App = ({ Component, pageProps }: AppProps): ReactElement => <Component {...pageProps} />
 
 export default App
